8338b91836ee384ac7721cf32819c8bcbf29e26f
Elias Leguizamon <elias.leguizamon1997@gmail.com>
2024-11-29 13:40:23 -0300

    create windows with iced

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,93 @@
-// src/main.rs
+use iced::{
+    alignment::{Horizontal, Vertical}, executor, widget::Text, Application, Color, Command, Element, Length, Settings, Subscription, Theme
+};
+use iced::time;
 
 mod structs;
 mod webhook;
 
-use tokio;
+pub fn main() -> iced::Result {
+    TofuApp::run(Settings {
+        window: iced::window::Settings {
+            size: iced::Size {
+                width: 800.0,
+                height: 600.0,
+            },
+            ..Default::default()
+        },
+        ..Default::default()
+    })
+}
 
-#[tokio::main]
-async fn main() {
-    // Suscripción a BTC/USDT y ETH/USDT
-    let btc_symbol = "btcusdt";
-    let xrp_symbol = "xrpusdt";
+struct TofuApp {
+    current_price: Option<String>,
+}
 
-    // Lanza las tareas de WebSocket
-    tokio::spawn(async move {
-        webhook::connect_to_binance(btc_symbol).await;
-    });
+#[derive(Debug, Clone)]
+enum Message {
+    PriceUpdated(String),
+    Tick,
+}
 
-    tokio::spawn(async move {
-        webhook::connect_to_binance(xrp_symbol).await;
-    });
+impl Application for TofuApp {
+    type Executor = executor::Default;
+    type Message = Message;
+    type Theme = Theme;
+    type Flags = ();
 
-    // Mantén el proceso activo indefinidamente
-    loop {
-        tokio::time::sleep(std::time::Duration::from_secs(60)).await;
+    fn new(_flags: ()) -> (Self, Command<Self::Message>) {
+        (
+            TofuApp {
+                current_price: None,
+            },
+            Command::perform(fetch_price(), Message::PriceUpdated),
+        )
+    }
+
+    fn title(&self) -> String {
+        String::from("Tofu")
+    }
+
+    fn update(&mut self, message: Self::Message) -> Command<Self::Message> {
+        match message {
+            Message::PriceUpdated(price) => {
+                self.current_price = Some(price);
+                Command::none()
+            }
+            Message::Tick => {
+                Command::perform(fetch_price(), Message::PriceUpdated)
+            }
+        }
+    }
+
+    fn subscription(&self) -> Subscription<Self::Message> {
+        time::every(std::time::Duration::from_secs(5)).map(|_| Message::Tick)
+    }
+
+    fn view(&self) -> Element<Self::Message> {
+        let price = self
+            .current_price
+            .as_deref()
+            .unwrap_or("Cargando precio...");
+
+        Text::new(price)
+            .size(60)
+            .horizontal_alignment(Horizontal::Center)
+            .vertical_alignment(Vertical::Center)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            // white text
+            .into()
+    }
+
+    fn theme(&self) -> Theme {
+        Theme::default()
+    }
+}
+
+async fn fetch_price() -> String {
+    match webhook::connect_to_ticker("xrpusdt").await {
+        Some(ticker) => ticker.last_price,
+        None => "0$".to_string(),
     }
 }
--- src/webhook.rs
+++ src/webhook.rs
@@ -1,8 +1,10 @@
 use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
 use futures_util::StreamExt;
 use crate::structs::{TradeResponse, TickerResponse};
+use std::thread::sleep;
 
-pub async fn connect_to_binance(symbol: &str) {
+#[allow(dead_code)]
+pub async fn connect_to_binance(symbol: &str) -> Option<TradeResponse> {
     let url = format!("wss://stream.binance.com:9443/ws/{}@trade", symbol.to_lowercase());
 
     let (ws_stream, _) = connect_async(url).await.expect("Failed to connect");
@@ -13,28 +15,30 @@ pub async fn connect_to_binance(symbol: &str) {
     while let Some(Ok(msg)) = read.next().await {
         if let Message::Text(text) = msg {
             let trade: TradeResponse = serde_json::from_str(&text).expect("Error deserializando trade");
-            println!("Nuevo Trade: {:#?}", trade);
+            return Some(trade);
         }
-        tokio::time::sleep(std::time::Duration::from_secs(2)).await;
+        sleep(std::time::Duration::from_secs(2));
     }
 
+    None
 }
 
 #[allow(dead_code)]
-pub async fn connect_to_ticker(symbol: &str) {
+pub async fn connect_to_ticker(symbol: &str) -> Option<TickerResponse> {
     let url = format!("wss://stream.binance.com:9443/ws/{}@ticker", symbol.to_lowercase());
 
     let (ws_stream, _) = connect_async(url).await.expect("Failed to connect");
     println!("Connected to WebSocket!");
 
-    let (mut write, mut read) = ws_stream.split();
+    let (_, mut read) = ws_stream.split();
 
-    // Esperamos mensajes de ticker
-    while let Some(Ok(msg)) = read.next().await {
+    if let Some(Ok(msg)) = read.next().await {
         if let Message::Text(text) = msg {
             let ticker: TickerResponse = serde_json::from_str(&text).expect("Error deserializando ticker");
-            println!("Ticker: {:#?}", ticker);
+            return Some(ticker);
         }
-        tokio::time::sleep(std::time::Duration::from_secs(2)).await;
     }
+
+    None
 }
+
